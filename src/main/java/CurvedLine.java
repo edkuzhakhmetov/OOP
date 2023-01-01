b2b76b46f27ea2fe9879f3f79dfefb2c0bbb93bd
edkuzhakhmetov <118476791+edkuzhakhmetov@users.noreply.github.com>
2023-01-02 01:53:26 +0500

    ломаная линия

--- src/main/java/CurvedLine.java
+++ src/main/java/CurvedLine.java
@@ -1,38 +1,92 @@
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//Создайте класс Ломаная, которая будет представлять собой ломаную линию (см. пример на рис.1), которая описывается:
+//массив Точек, через которые линия проходит
 public class CurvedLine {
-List<Point> pointArr=new ArrayList<>();
+    Point[] pointArr;
+    Line line;
+    List<Line> linesArr;
 
+    //При создании объекта можно ничего не указывать
+    public CurvedLine() {
+        //Значения по умолчанию
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(3, 3);
+        Point p3 = new Point(2, 2);
+        Point p4 = new Point(10, 10);
+        //Линия 1 начиналась в Точке  {1;3} и кончалась в Точке {5;8}.
+        Point[] arr = {p1, p2, p3, p4};
+        this.pointArr = arr;
+        LineCreate(pointArr);
+    }
+
+    //При создании объекта можно указать начальный набор Точек
     public CurvedLine(Point[] pointArr) {
         this.pointArr = pointArr;
+        LineCreate(pointArr);
+        /*
+        for (int i = 0; i < pointArr.length-1;) {
+            line = new Line(pointArr[i],pointArr[++i]);
+            System.out.println(line);
+        }
+
+         */
     }
 
-    public CurvedLine() {
-        pointArr =[1,1,4,4,2,2,3,3];
+    //Создание линий из массива
+    public void LineCreate(Point[] pointArr) {
+        int i;
+        linesArr = new ArrayList<>();
+        for (i = 0; i < this.pointArr.length - 1; ) {
+            line = new Line(pointArr[i], pointArr[++i]);
+            linesArr.add(line);
+        }
+        //System.out.println(linesArr);
     }
 
+    //Может вернуть массив точек,если это имелось ввиду
+    public Point[] getPointArr() {
+        return pointArr;
+    }
+
+    //Может вернуть массив Линий
+    public List<Line> getLinesArr() {
+        return linesArr;
+    }
+
+    // Может вернуть свою длину
+//
+// Рассчитать длину массива Линий
+    public double getLineArrLength() {
+        double sumLength = 0;
+        int j;
+        for (j = 0; linesArr.length; ) {
+            sumLength += Line.getLineLength();
+            j++;
+        }
+        return sumLength;
+    }
+
+    //Может быть приведена к строковой форме вида “Линия [Т1,T2,…,TN]”, где TN – это результат приведения к строке Точки с номером N
     @Override
     public String toString() {
-        return "CurvedLine{" +
-                "pointArr=" + Arrays.toString(pointArr) +
-                '}';
+        return "Ломаная Линия " + Arrays.toString(pointArr);
     }
 }
 /*
-Создайте класс Ломаная, которая будет представлять собой ломаную линию (см. пример на рис.1), которая описывается:
 
-массив Точек, через которые линия проходит
-При создании объекта можно ничего не указывать, или указать начальный набор Точек
-Может быть приведена к строковой форме вида “Линия [Т1,T2,…,TN]”, где TN – это результат приведения к строке Точки с номером N
-Может вернуть массив Линий
-Может вернуть свою длину
+
+
+
+
 Под Точкой имеется ввиду класс, созданный в задании 1 (точка координат). Под Линией имеется ввиду класс, созданный в задании 2 (линия).
 
 Необходимо выполнить следующие задачи:
 
-Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
+
 Рассчитать длину Ломаной
 Получить у Ломаной массив Линий
 Рассчитать длину массива Линий
--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,17 +1,24 @@
+import java.util.Arrays;
+
 public class Start {
     public static void main(String[] args) {
-        Point p1 = new Point(1, 3);
-        Point p2 = new Point(5, 8);
-        Point p3 = new Point(10, 11);
-        Point p4 = new Point(15, 19);
-        //Линия 1 начиналась в Точке  {1;3} и кончалась в Точке {5;8}.
-        CurvedLine line1 = new CurvedLine();//с указанием двух Точек
-        //Линия 2 начиналась в Точке {10;11} и кончалась в Точке {15;19}
-        Line line2 = new Line(p3, p4);//с указанием двух Точек
+        Point p1 = new Point(1, 5);
+        Point p2 = new Point(2, 8);
+        Point p3 = new Point(5, 3);
+        Point p4 = new Point(8, 9);
+        //Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
+        Point[] arr= {p1,p2,p3,p4};
+
+        CurvedLine cline1 = new CurvedLine(arr);
+
         //Линия 3 начиналась в Точке конца Линии 1, а кончалась в Точке начала Линии 2
-        Line line3 = new Line(p2, p3);//с указанием двух Точек
+        //Line line3 = new Line(p2, p3);//с указанием двух Точек
         //Используя эти объекты, выполните следующее:
         //Выведите текстовое представление Линии 3 на экран
-        System.out.println(line1);
+        CurvedLine cline2 = new CurvedLine();
+        System.out.println(cline1);
+        System.out.println(cline1.getLinesArr());
+        System.out.println(cline1.getLineArrLength());
+        //System.out.println(Arrays.toString(cline1.getPointArr()));
     }
 }
