8c5628e3331f189bdbfbf78091074121de53c3b4
edkuzhakhmetov <118476791+edkuzhakhmetov@users.noreply.github.com>
2023-01-04 17:10:46 +0500

    Инкапсуляция Студент

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,26 +1,21 @@
 public class Start {
     public static void main (String[] args){
-        Point point1=new Point (1, 3);
-        Point point2=new Point (1, 3);
-        Point point3=new Point (5, 6);
-        //Выведите на экран текстовое представление этих точек
-        System.out.println(point1);
-        System.out.println(point2);
-        System.out.println(point3);
-        //результат сравнения точек между собой 1 и 2
-        if (point1.x==point2.x&&point1.y==point2.y){
-            System.out.println("Точка "+point1+" равна точке "+point2);
-        }
-        else System.out.println("Точка "+point1+" не равна точке "+point2);
-        //результат сравнения точек между собой 1 и 3
-        if (point1.x==point3.x&&point1.y==point3.y){
-            System.out.println("Точка "+point1+" равна точке "+point3);
-        }
-        else System.out.println("Точка "+point1+" не равна точке "+point3);
-
-        if (point2.x==point3.x&&point2.y==point3.y){
-            System.out.println("Точка "+point2+" равна точке "+point3);
-        }
-        else System.out.println("Точка "+point2+" не равна точке "+point3);
+        //1
+        int [] arr={5,5,3,4,2,4};
+        Student s1 =new Student("Ant",arr);
+        System.out.println(s1);
+        //2
+        Student s2 =new Student("Ivan");
+        System.out.println(s2);
+        //
+        s2.addScores(arr);
+        System.out.println(s2);
+        //System.out.println(s1.getScoresList()==s2.getScoresList());
+        //
+        int [] arr2={2,3};
+        s1.setName("Anton");
+        s1.addScores(arr2);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
